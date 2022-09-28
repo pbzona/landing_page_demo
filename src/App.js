a429bb8c77b1eb3cb3e7a198cd62693fb10ca41f
Phil Zona <pzona@launchdarkly.com>
2022-09-28 11:50:24 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import Logo from './components/Logo';
 import TextBox from './components/TextBox';
 import './css/global.css';
 
-import { useFlags } from 'launchdarkly-react-client-sdk';
+import { useFlags, useLDClient } from 'launchdarkly-react-client-sdk';
 
 function App() {
-  const { showBanner, showIncidentMessage } = useFlags();
+  const { showBanner, showIncidentMessage, showLiveMessage } = useFlags();
 
   const flags = useFlags();
+  const client = useLDClient();
 
-  console.log(flags);
+  // console.log(flags);
+  console.log(client.getUser());
+  console.log(showBanner);
   return (
     <>
       {showBanner && <Banner isIncident={showIncidentMessage} />}
@@ -21,6 +24,7 @@ function App() {
         <Logo />
         <TextBox />
         <ConvertButton />
+        {showLiveMessage && <p style={{fontSize: 64, color: 'red'}}>TESTING LIVE!</p>}
       </main>
     </>
   );
